6d6d21034f4c1c845b7612dbdbccf364095061d6
moskanoro <mostapha.boudad.02@gmail.com>
2024-02-29 10:52:05 +0800

    correction 1st task

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,24 +1,24 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Create a binary node
+ * @parent: node parent
+ * @value: Value of the node
+ * Return: the new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
+}
